559770c4c8443f14c258856cbf776ab456dd6012
Viktor <vtitch@gmail.com>
2020-05-01 22:09:45 +0200

    wss

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ import '@/services/exchangeSockets/new_bitfinex';
 //import '@/services/exchangeSockets/bitmex.js';
 import '@/services/exchangeSockets/newApiEngine';
 
-const device = new WebSocket('ws://trade.coinbae.org:8090');
+const device = new WebSocket('wss://trade.coinbae.org/ws/');
 
 
 device.onopen = () => {
