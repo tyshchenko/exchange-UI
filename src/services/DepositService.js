939b1af49a069cb9d97189670e788c7d69239c45
Viktor <vtitch@gmail.com>
2020-05-04 17:22:38 +0200

    Update DepositService.js

--- src/services/DepositService.js
+++ src/services/DepositService.js
@@ -3,7 +3,7 @@ import ApiCurryBase from './ApiCurryBase';
 class DepositService {
   async fetchDespositAddress(userid, currency) {
     let method = 'user.deposit_address';
-    return (await ApiCurryBase.post('/api/', {
+    return (await ApiCurryBase.post('/', {
       userid,
       currency,
       method,
