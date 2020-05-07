44d80655a8f567a2cf17af353ba7872f572e4cce
Viktor <vtitch@gmail.com>
2020-05-07 22:36:00 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Settings from '@/components/Settings/Settings.vue';
 import History from '@/components/History/History.vue';
 import BalanceTransfer from '@/components/BalanceTransfer/BalanceTransfer.vue';
 import SummaryBitfinex from '@/components/BalanceTransfer/SummaryBitfinex/SummaryBitfinex.vue';
+import WalletSummary from '@/components/WalletSummary/WalletSummary.vue';
 import ActiveTradesMobile from '@/components/ActiveTradesMobile/ActiveTradesMobile.vue';
 import LocalStorage from '@/utils/localStorage.js';
 import EventBus, {
