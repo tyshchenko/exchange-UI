53a838d033fa47c76e15520c17ccbb1f06bff6bf
Viktor <vtitch@gmail.com>
2020-05-07 23:51:17 +0200

    update

--- src/components/Settings/SendMoney/SendMoney.vue
+++ src/components/Settings/SendMoney/SendMoney.vue
@@ -1,83 +1,158 @@
 <template src="./template.html"></template>
 
 <script>
-import TrezorService from '@/services/TrezorService.js';
+import { stringArrayToHtmlList, } from '@/utils/utility';
+import WithdrawlService from '@/services/WithdrawlService';
 
 export default {
   name: 'send-money',
-  components: {},
   data() {
     return {
-      selectedCurrency: 'SELECT A CURRENCY',
-      displayFlag: true,
-      address: '',
-      amount: 0,
-      currencyArray: [{key: 'btc', value: 'Bitcoin',}, {key: 'bch', value: 'Bitcoin Cash',}, {key: 'btg', value: 'Bitcoin Gold',}, {key: 'ltc', value: 'Litecoin',}, {key: 'dash', value: 'Dash',}, {key: 'zcash', value: 'Zcash',}, {key: 'test', value: 'Testnet',}, {key: 'doge', value: 'Dogecoin',}, {key: 'vtc', value: 'Vertcoin',},],
-      result: {
-        signatures: [],
-        serializedTx: '',
-        txid: '',
+      supportedCoins: ['btc', ],
+      formValue: {
+        amount: '',
+        recievingAddress: '',
+        coin: 'btc',
+        emailOtp: '',
+        id: '',
       },
-      routes: [
-        {
-          amount: '',
-          address: '',
-        },
-      ],
+      withdrawalFee: 0,
+      otpSent: false,
+      statuses: {
+        btc: this.$store.getters.getBtcStatus,
+        ltc: this.$store.getters.getLtcStatus,
+        bch: this.$store.getters.getBchStatus,
+        xmr: this.$store.getters.getXmrStatus,
+      },
+      fees: {
+        btc: 0,
+        bch: 0,
+        ltc: 0,
+      },
     };
   },
-  mounted() {
+  watch: {
+    'formValue.coin': {
+      handler: function(to) {
+        this.showFees(to);
+      },
+      deep: true,
+    },
+  },
+  async created() {
+    let fees = (await WithdrawlService.getWithdrawalFees()).data;
+    fees.forEach((element) => {
+      this.fees[element.currency] = parseFloat(element.fees.toString());
+    });
+    this.withdrawalFee = this.fees[this.formValue.coin];
   },
   methods: {
-    goBack () {
-      this.displayFlag = true;
-      this.selectedCurrency = 'SELECT A CURRENCY';
-      this.address = '';
-      this.amount = '';
-      this.result = {
-        signatures: [],
-        serializedTx: '',
-        txid: '',
-      };
+    getStatus(coin) {
+      if(coin === 'btc') {
+        return  this.$store.getters.getBtcStatus;
+      } else  if(coin === 'bch') {
+        return  this.$store.getters.getBchStatus;
+      } else if(coin === 'ltc'){
+        return  this.$store.getters.getLtcStatus;
+      } else if(coin === 'xmr'){
+        return  this.$store.getters.getXmrStatus;
+      }
     },
-    changeSelectedCurrency(key) {
-      this.selectedCurrency = key;
+    getGrpId(index) {
+      return 'grp-a' + index;
     },
-    addNewForm() {
-      this.routes.push({
-        amount: '',
-        address: '',
+    async withdrawCrypto() {
+      this.$store.commit('addLoaderTask', 1, false);
+      this.formValue.id = `${Date.now()}`;
+      let response;
+      let validationErrors = [];
+      // let isAmountValid = true;
+      // let isCoinValid = true;
+      if (!(Number(this.formValue.amount) > 0)) {
+        validationErrors.push('Invalid Amount');
+        // isAmountValid = false;
+      }
+
+      if (!this.formValue.recievingAddress)
+        validationErrors.push('Recieving Address is required');
+
+      if (this.supportedCoins.indexOf(this.formValue.coin) < 0) {
+        validationErrors.push('Coin not supported');
+        // isCoinValid = false;
+      }
+
+      // if (!validationErrors.length) {
+      //   response = await DepositService.generateWallet({ coin, });
+      //   if (!response.status) {
+      //     validationErrors.push(...(response.data.errors || []));
+      //   }
+      // }
+      if (!validationErrors.length) {
+        response = await WithdrawlService.sendOTP(this.formValue);
+        if (!response.status) {
+          validationErrors.push(...(response.data.errors || []));
+        }
+      }
+      if (validationErrors.length) {
+        this.$showErrorMsg({
+          message: stringArrayToHtmlList(validationErrors),
+        });
+      } else {
+        this.otpSent = true;
+        this.$showInfoMsg({
+          message: 'OTP Sent to your registered Email Id.',
+        });
+      }
+      this.$store.commit('removeLoaderTask', 1);
+    },
+    cancelWithdrawl() {
+      this.formValue.recievingAddress = '';
+      this.formValue.amount = '';
+      this.formValue.emailOtp = '';
+      this.otpSent = false;
+      this.$showInfoMsg({
+        message: 'Withdrawl request cancelled by user.',
       });
     },
-    deleteKey(index) {
-      this.routes.splice(index, 1);
-    },
-
-    async handleSubmit() {
-      try{
-        let response = await TrezorService.composeTransaction(this.routes, this.selectedCurrency, true);
-        if(response.success) {
-          this.displayFlag = false;
-          this.result.txid = response.payload.txid;
-          this.result.signatures = response.payload.signatures;
-          this.result.serializedTx = response.payload.serializedTx;
-        }
-        else {
-          this.selectedCurrency= 'SELECT A CURRENCY';
-          this.routes = [{amount: '', address: '',},];
-          this.$showErrorMsg({
-            message: `${response.payload.error}`,
-          });
-        }
-      }catch(err){
-        this.$showErrorMsg({
-          message: 'OOPS .... There was an Error',
-        });
-        return err;
+    async confirmWithdrawl() {
+      this.$store.commit('addLoaderTask', 1, false);
+      let validationErrors = [];
+      if (this.formValue.emailOtp === '') {
+        validationErrors.push('Please enter the otp recieved.');
       }
+      if (!validationErrors.length) {
+        let response = await WithdrawlService.withdrawCrypto(this.formValue);
+        if (!response.status) {
+          validationErrors.push(...(response.data.errors || []));
+        }
+      }
+      if (validationErrors.length) {
+        this.$showErrorMsg({
+          message: stringArrayToHtmlList(validationErrors),
+        });
+      } else {
+        this.formValue.recievingAddress = '';
+        this.formValue.amount = '';
+        this.formValue.emailOtp = '';
+        this.otpSent = false;
+        this.$showSuccessMsg({
+          message: 'Withdrawl request successful',
+        });
+      }
+      this.$store.commit('removeLoaderTask', 1);
+    },
+    async showFees(coin) {
+      this.withdrawalFee = this.fees[coin];
     },
   },
 };
 </script>
-
+<style lang="scss" scoped>
+.resend {
+  line-height: 40px;
+}
+.resend :hover {
+  color: var(--primary-text-color);
+}
+</style>
 <style lang="scss" src="./style.scss" scoped></style>
