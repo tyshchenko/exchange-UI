d39254984e19f50d548229ab01438ad605a569a4
Viktor <vtitch@gmail.com>
2020-05-07 20:39:42 +0200

    Update HistoryService.js

--- src/services/HistoryService.js
+++ src/services/HistoryService.js
@@ -1,11 +1,11 @@
 import ApiCurryBase from './ApiCurryBase';
 import LocalStorage, { Keys, } from '@/utils/localStorage.js';
-const mqttKey = LocalStorage.get(Keys.mqtt);
 import EventBus, { EventNames, } from '@/eventBuses/default';
 
 class History {
   async bitfinexTradingHistoryData(exchange) {
     // console.log((await ApiCurryBase.post('/bitfinex-wallet-transfer-request', body)).data)
+    let mqttKey = LocalStorage.get(Keys.mqtt);
     return (await ApiCurryBase.post('/', {'method': 'order.penrding','id':1, 'params':[mqttKey,exchange,],})).data;
   }
   async bitfinexTransactionHistoryData(exchange) {
@@ -18,7 +18,12 @@ class History {
   }
   async depositTransactionHistoryData() {
     // console.log((await ApiCurryBase.post('/bitfinex-wallet-transfer-request', body)).data)
-    return (await ApiCurryBase.post('/', {'method': 'deposit.history','id':1, 'params':[mqttKey,],})).data;
+    let mqttKey = LocalStorage.get(Keys.mqtt);
+    let responce = (await ApiCurryBase.post('/', {'method': 'deposit.history','id':1, 'params':[mqttKey,],})).data;
+    if (responce.Expired==1) {
+      EventBus.$emit(EventNames.userLogout);
+    }
+    return responce;
   }
   async bitfinexOrdersHistoryData() {
     // console.log((await ApiCurryBase.post('/bitfinex-wallet-transfer-request', body)).data)
