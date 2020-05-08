3005edde36f003ec0f28097a947082ba2b64c7a0
Viktor <vtitch@gmail.com>
2020-05-08 10:24:47 +0200

    Update TradeService.js

--- src/services/TradeService.js
+++ src/services/TradeService.js
@@ -81,22 +81,24 @@ class TradeService {
     let data = response.data;
     if (data.Expired==1) {
       EventBus.$emit(EventNames.userSessionExpired);
+      return {data:[],};
+    } else {
+      let outputdata = data.result.records.map(rt => ({
+        id: rt.id,
+        clientOrderId: rt.id,
+        orderId: rt.id,
+        placedTime: rt.ctime,
+        amount: rt.amount,
+        avgPrice: rt.price,
+        buyOrSell: rt.side==2 ? 'buy' : 'sell',
+        exchange: 'XCoinBae',
+        orderType: '',
+        stopPrice:  rt.price,
+        status: rt.deal_stock>0 ? 'part.filled' : 'pending',
+        pair: rt.market,
+      }));
+      return {data:outputdata,};
     }
-    let outputdata = data.result.records.map(rt => ({
-      id: rt.id,
-      clientOrderId: rt.id,
-      orderId: rt.id,
-      placedTime: rt.ctime,
-      amount: rt.amount,
-      avgPrice: rt.price,
-      buyOrSell: rt.side==2 ? 'buy' : 'sell',
-      exchange: 'XCoinBae',
-      orderType: '',
-      stopPrice:  rt.price,
-      status: rt.deal_stock>0 ? 'part.filled' : 'pending',
-      pair: rt.market,
-    }));
-    return {data:outputdata,};
   }
   
   async getOpenPositions(exchange) {
