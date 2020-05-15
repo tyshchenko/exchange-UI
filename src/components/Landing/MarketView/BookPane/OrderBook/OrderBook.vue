1fafc0dd884cfb35968811581683e7b1108e66ca
Viktor <vtitch@gmail.com>
2020-05-15 18:24:52 +0200

    Update OrderBook.vue

--- src/components/Landing/MarketView/BookPane/OrderBook/OrderBook.vue
+++ src/components/Landing/MarketView/BookPane/OrderBook/OrderBook.vue
@@ -141,7 +141,7 @@ export default {
       this.bids = this.bidsUpdater(parsedSnap);
       this.barAsk = this.asks[0].totalVolume;
       this.barBid = this.bids[this.bids.length - 1].totalVolume;
-      this.$store.commit('liquidity', Math.floor(this.barAsk+this.barBid));
+      this.$store.commit('liquidity', Math.floor(this.asks[this.asks.length - 1].totalVolume+this.bids[0].totalVolume));
       this.showLoader = false;
     };
     this.reset = () => {
