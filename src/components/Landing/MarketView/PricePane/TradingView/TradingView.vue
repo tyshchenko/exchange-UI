60707cab1f1bebd418a5edb697cb425e434c2f61
Viktor <vtitch@gmail.com>
2020-12-02 16:56:14 +0100

    tv

--- src/components/Landing/MarketView/PricePane/TradingView/TradingView.vue
+++ src/components/Landing/MarketView/PricePane/TradingView/TradingView.vue
@@ -2,52 +2,116 @@
 
 <script>
 import ExchangeDataEventBus from '@/eventBuses/exchangeData';
+import { widget } from './charting_library.standalone';
+
+function getLanguageFromURL() {
+  const regex = new RegExp('[\\?&]lang=([^&#]*)');
+  const results = regex.exec(window.location.search);
+  return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+
+
 export default {
   name: 'trading-view',
-  data() {
-    return {
-      exchange: this.$store.state.selectedExchange,
-      bitmexPathVariable: '',
-    };
-  },
-  mounted() {
-    if(this.exchange === 'bitmex') {
-      this.bitmexPathVariable = `https://bitmex.blocknomic.com/index.html?symbol=BITMEX%3A${this.$store.state.selectedPair.toUpperCase()}&exchange=bitmex`;
-    } else if(this.exchange === 'bitfinex') {
-      let str1 = this.$store.state.selectedPair.toUpperCase();
-      let res1 = str1.split('/');
-      let noSlashPairBitfinex = res1[0] + res1 [1];
-      this.bitmexPathVariable = `https://bitmex.blocknomic.com/index.html?symbol=BITFINEX%3A${noSlashPairBitfinex.toUpperCase()}&exchange=bitfinex`;
-    } else if(this.exchange === 'binance') {
-      let str2 = this.$store.state.selectedPair.toUpperCase();
-      let res2 = str2.split('/');
-      if(res2[0] === 'USD') {
-        res2[0] = 'USDT';
-      } else if(res2[1] === 'USD') {
-        res2[1] = 'USDT';
-      }
-      let noSlashPairBinance = res2[0] + res2 [1];
-      this.bitmexPathVariable = `https://bitmex.blocknomic.com/index.html?symbol=BINANCE%3A${noSlashPairBinance.toUpperCase()}&exchange=binance`;
-    } else if(this.exchange === 'bequant') {
-      let str3 = this.$store.state.selectedPair.toUpperCase();
-      let res3 = str3.split('/');
-      if(res3[0] === 'USD') {
-        res3[0] = 'USDT';
-      } else if(res3[1] === 'USD') {
-        res3[1] = 'USDT';
-      }
-      let noSlashPairHitbtc = res3[0] + res3 [1];
-      this.bitmexPathVariable = `https://bitmex.blocknomic.com/index.html?symbol=HITBTC%3A${noSlashPairHitbtc.toUpperCase()}&exchange=bequant`;
+  props: {
+    symbol: {
+      default: 'AAPL',
+      type: String,
+    },
+    interval: {
+      default: 'D',
+      type: String,
+    },
+    containerId: {
+      default: 'tv_chart_container',
+      type: String,
+    },
+    datafeedUrl: {
+      default: 'https://demo_feed.tradingview.com',
+      type: String,
+    },
+    libraryPath: {
+      default: '/charting_library/',
+      type: String,
+    },
+    chartsStorageUrl: {
+      default: 'https://saveload.tradingview.com',
+      type: String,
+    },
+    chartsStorageApiVersion: {
+      default: '1.1',
+      type: String,
+    },
+    clientId: {
+      default: 'tradingview.com',
+      type: String,
+    },
+    userId: {
+      default: 'public_user_id',
+      type: String,
+    },
+    fullscreen: {
+      default: false,
+      type: Boolean,
+    },
+    autosize: {
+      default: true,
+      type: Boolean,
+    },
+    studiesOverrides: {
+      type: Object,
     }
   },
-  methods: {
+  tvWidget: null,
+  mounted() {
+    const widgetOptions = {
+      symbol: this.symbol,
+      // BEWARE: no trailing slash is expected in feed URL
+      datafeed: new window.Datafeeds.UDFCompatibleDatafeed(this.datafeedUrl),
+      interval: this.interval,
+      container_id: this.containerId,
+      library_path: this.libraryPath,
+      locale: getLanguageFromURL() || 'en',
+      disabled_features: ['use_localstorage_for_settings'],
+      enabled_features: ['study_templates'],
+      charts_storage_url: this.chartsStorageUrl,
+      charts_storage_api_version: this.chartsStorageApiVersion,
+      client_id: this.clientId,
+      user_id: this.userId,
+      fullscreen: this.fullscreen,
+      autosize: this.autosize,
+      studies_overrides: this.studiesOverrides,
+    };
+    const tvWidget = new widget(widgetOptions);
+    this.tvWidget = tvWidget;
+    tvWidget.onChartReady(() => {
+      tvWidget.headerReady().then(() => {
+        const button = tvWidget.createButton();
+        button.setAttribute('title', 'Click to show a notification popup');
+        button.classList.add('apply-common-tooltip');
+        button.addEventListener('click', () => tvWidget.showNoticeDialog({
+            title: 'Notification',
+            body: 'TradingView Charting Library API works correctly',
+            callback: () => {
+              // eslint-disable-next-line no-console
+              console.log('Noticed!');
+            },
+          }));
+        button.innerHTML = 'Check API';
+      });
+    });
   },
   destroyed() {
+    if (this.tvWidget !== null) {
+      this.tvWidget.remove();
+      this.tvWidget = null;
+    }
     ExchangeDataEventBus.$emit('unsubscribe-candles');
     ExchangeDataEventBus.$off('snapshotCandles');
-  },
+  }
 };
 </script>
 
 
-// <style lang="scss" src="./style.scss" scoped></style>
+<style lang="scss" src="./style.scss" scoped></style>
