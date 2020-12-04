f4fae69d4bdb153a2aff2295e001964df2618b7c
Viktor <vtitch@gmail.com>
2020-12-04 09:39:06 +0100

    Update OrderBook.vue

--- src/components/Landing/MarketView/BookPane/OrderBook/OrderBook.vue
+++ src/components/Landing/MarketView/BookPane/OrderBook/OrderBook.vue
@@ -108,8 +108,8 @@ export default {
       }
     },
     roundPriceByPrecision(price, precision) {
-      let roundArray = [0,1,10,100,];
-      let fractionDigitsArray = [1,1,1,1,];
+      let roundArray = [100,10000,1000000,100000000,];
+      let fractionDigitsArray = [2,4,6,8,];
       //returns price according to precesion
       let fractionDigits = fractionDigitsArray[precision];
       let round = roundArray[precision];
