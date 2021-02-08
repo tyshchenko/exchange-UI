3240ecfd11a175392dc28e8c16ae31c96056b4c3
Viktor <vtitch@gmail.com>
2021-02-08 15:23:07 +0100

    Update KYC.vue

--- src/components/Settings/KYC/KYC.vue
+++ src/components/Settings/KYC/KYC.vue
@@ -14,7 +14,12 @@ export default {
       activeTab: 'KYC',
       email:loggedInUser,
       displayFlag: false,
-      kyc:{firstname:'',lastname:'',phone:'',country:'',},
+      kyc: {
+        firstname:'',
+        lastname:'',
+        phone:'',
+        country:'',
+        },
     };
   },
   async created() {
@@ -25,7 +30,14 @@ export default {
       /* eslint-enable no-console */
 
     } else {
+      /* eslint-disable no-console */
+      console.log(this.kyc);
+      /* eslint-enable no-console */
       this.kyc = data.data;
+      /* eslint-disable no-console */
+      console.log(data.data);
+      console.log(this.kyc);
+      /* eslint-enable no-console */
     }
   },
   methods: {
